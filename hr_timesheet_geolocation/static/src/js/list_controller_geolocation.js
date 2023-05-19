722b37081eba499461f2e89f64a2680ddf17dc14
Swapnesh Shah <swapnesh@prospectsmarttech.com>
2023-05-19 15:55:33 +0530

    [MIG][1091]hr_timesheet_geolocation: migration to V16

--- hr_timesheet_geolocation/static/src/js/list_controller_geolocation.js
+++ hr_timesheet_geolocation/static/src/js/list_controller_geolocation.js
@@ -1,32 +1,23 @@
-odoo.define("hr_timesheet_geolocation.ListControllerLocation", function (require) {
-    "use strict";
+/** @odoo-module */
 
-    /* eslint no-unused-vars: ["error", { "args": "none" }]*/
+ /* eslint no-unused-vars: ["error", { "args": "none" }]*/
 
-    var ListController = require("web.ListController");
-    var session = require("web.session");
+import { ListController } from "@web/views/list/list_controller";
+import { patch } from '@web/core/utils/patch';
 
-    ListController.include({
-        /**
-         * @override
-         * @param {Widget} parent
-         * @param {GraphModel} model
-         * @param {GraphRenderer} renderer
-         * @param {Object} params
-         * @param {String[]} params.measures
-         * @param {Boolean} params.isEmbedded
-         * @param {String[]} params.groupableFields,
-         */
-        init: function (parent, model, renderer, params) {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    session.user_context.default_analytic_latitude =
-                        position.coords.latitude;
-                    session.user_context.default_analytic_longitude =
-                        position.coords.longitude;
-                });
-            }
-            return this._super.apply(this, arguments);
-        },
-    });
+patch(ListController.prototype, 'hr_timesheet_geolocation_ListController', {
+    setup() {
+        var self = this;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                if(self.props && self.props.context) {
+                    self.props.context.default_analytic_latitude =
+                            position.coords.latitude;
+                    self.props.context.default_analytic_longitude =
+                            position.coords.longitude;
+                }
+            });
+        }
+        this._super.apply(this, arguments);
+    }
 });
